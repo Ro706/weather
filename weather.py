14dd35079582261a2b757edc6cf37033a3834c0a
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2021-05-22 10:40:24 +0530

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,31 @@
+from pyfiglet import Figlet
+from gtts import gTTS as gt
+import requests
+import json
+import os
+f = Figlet(font='slant')
+print (f.renderText('weather report'))
+City = str(input('enter your city name: '))
+api_key ='04018081b69cca6f721c5ed1a46be071'
+base_url = 'https://api.openweathermap.org/data/2.5/weather?'
+url = base_url+'appid='+api_key+'&q='+City+'&units=metric'
+response = requests.get(url)
+x=response.json()
+if x['cod']!=401:
+    print ('city name:',x['name'])
+    print ('weather:',x['weather'])
+    print ('weather:',x['weather'][0]['main'])
+    print ('temp:',x['main']['temp'])
+    print ('minimum temp:',x['main']['temp_min'])
+    print ('max temp:',x['main']['temp_max'])
+else:
+    print ('city not found')
+a = x['weather'][0]['main']
+b = x['main']['temp']
+c = x['main']['temp_min']
+d = x['main']['temp_max']
+weather_report=f'It mostly {a} today.Current temperature in {City} is {b}.High of {c} and low of {d} .Thank u sir'
+language = 'en'
+output = gt(text = weather_report,lang = language)
+output.save('weather.mp3')
+os.system('mpv /data/data/com.termux/files/home/weather')
